b90cabf18429ac5166c037293f81ce777b05e77e
Yoonje Choi <yoonjechoi@gmail.com>
2017-03-22 15:15:29 +0900

    feat(instagram crawler): add Instagram crawler command and tests for it
    https://app.asana.com/0/297074750378486/299687767035791

---
+++ selstagram_server/selsta101/test_instagram_crawler.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+
+from selstagram_server import utils
+from .management.commands.crawl import InstagramCrawler
+
+
+class InstagramCrawlerTest(TestCase):
+    def test_instagram_crawler(self):
+        # Given : login to Instagram as username '3tims3meals'
+        username = 'your_username'
+        password = 'your_password'
+        tag = 'selfie'
+        crawler = InstagramCrawler(directory=None,
+                                   profile=None,
+                                   hashtag=tag,
+                                   add_metadata=False,
+                                   get_videos=False,
+                                   videos_only=False)
+
+        self.assertRaises(ValueError, crawler.login, username, password)
+
+        # FIXME
+        # After edit username, password value above,
+        # remove # mark for code below and comment out code above assertion
+
+        # crawler.login(username, password)
+        #
+        # # When : Crawling 40 instagram today's photos tagged by selfie
+        # today = utils.BranchUtil.today()
+        # count = 40
+        #
+        # number_of_media = 0
+        # for _ in crawler.medias(media_count=count, timeframe=(today, today)):
+        #     number_of_media += 1
+        #
+        # # Then : number of photos crawled is 40
+        # self.assertEqual(count, number_of_media)
---
+++ selstagram_server/selstagram_server/utils.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import datetime
+from dateutil.relativedelta import relativedelta
+from pytz import timezone as pytz_timezone
+from django.utils import timezone as django_timezone
+
+
+class BranchUtil(object):
+    SEOUL_TIMEZONE = pytz_timezone('Asia/Seoul')
+    UTC = pytz_timezone('UTC')
+
+    @staticmethod
+    def yesterday():
+        return BranchUtil.today() + relativedelta(days=-1)
+
+    @staticmethod
+    def today():
+        return BranchUtil.now().date()
+
+    @staticmethod
+    def tomorrow():
+        return BranchUtil.today() + relativedelta(days=1)
+
+    @staticmethod
+    def now():
+        now = django_timezone.now()
+        return BranchUtil.to_seoul(now)
+
+    @classmethod
+    def date_to_datetime(cls, date):
+        return cls.localize(datetime.datetime.combine(date, datetime.time.min))
+
+    @classmethod
+    def localize(cls, dt):
+        return cls.SEOUL_TIMEZONE.localize(dt)
+
+    @classmethod
+    def to_utc(cls, dt=None):
+        if dt is None:
+            dt = BranchUtil.now()
+
+        return dt.astimezone(cls.UTC)
+
+    @classmethod
+    def to_seoul(cls, dt):
+        return dt.astimezone(cls.SEOUL_TIMEZONE)
+
+    @classmethod
+    def calc_utc_min_max_datetime(cls, date=None):
+        if date is None:
+            date = BranchUtil.today()
+
+        dt = cls.date_to_datetime(date)
+        from_datetime = datetime.datetime.combine(dt.date(), datetime.time.min)
+        from_datetime = cls.localize(from_datetime)
+        from_datetime = cls.to_utc(from_datetime)
+
+        to_datetime = datetime.datetime.combine(dt.date(), datetime.time.max)
+        to_datetime = cls.localize(to_datetime)
+        to_datetime = cls.to_utc(to_datetime)
+
+        return from_datetime, to_datetime
